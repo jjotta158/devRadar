c1e00d713929bbdfc17835e892511761d6419fd5
Jairo <jairo@agti.en.br>
2020-01-18 17:02:21 -0200

    Correção de erros na api o backend

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -4,13 +4,14 @@ const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
     async store(req,res) {
-        const { github_username, techs, latitude, longitude } = request.body;
+        const { github_username, techs, latitude, longitude } = req.body;
         let dev = await Dev.findOne({github_username});
 
         if (!dev) {
             const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
             const { name = login , avatar_url, bio } = apiResponse.data;
             const techsArray = parseStringAsArray(techs);
+            
             const location = {
                 type:'Point',
                 coordinates:[longitude, latitude],
@@ -20,7 +21,7 @@ module.exports = {
                 name,
                 avatar_url,
                 bio,
-                techs:techsArray,
+                tech:techsArray,
                 location
             })
         }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,23 @@
-const express = require('express');
-const mongoose = require("mongoose");
-const routes = require('./routes');
-const cors = require('cors');
-const app = express();
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const http = require('http')
 
-mongoose.connect("mongodb+srv://omnistack:LesBleus2018@cluster0-cbgb5.mongodb.net/test?retryWrites=true&w=majority",{
+mongoose.connect('mongodb+srv://omnistack:LesBleus2018@cluster0-cbgb5.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
-    useUnifiedTopology:true
-});
+    useUnifiedTopology: true
+})
+
+const app = express()
+const server = http.Server(app)
+
+const routes = require('./routes.js')
+//const { setupWebsocket } = require('./websocket.js')
+
+//setupWebsocket(server)
 
 app.use(cors())
-app.use(express.json);
-app.use(routes);
+app.use(express.json())
+app.use(routes)
 
-app.listen(3333);
+server.listen(3333)
